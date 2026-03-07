88b9f4ed60334e2d471bd75f34241344b1c6a152
S-Nikhil-Prince <23a91a0459@aec.edu.in>
2026-03-07 10:10:41 +0530

    updated the virus

--- Programs/make virus/virus.bat
+++ Programs/make virus/virus.bat
@@ -6,7 +6,7 @@ cls
 echo Microsoft Windows Security Scanner
 echo -----------------------------------
 echo Initializing Virus ...
-timeout /t 3 >nul
+timeout /t 1 >nul
 
 echo Corrupting system files...
 timeout /t 1 >nul
@@ -21,6 +21,7 @@ set /a percent=0
 set /a percent=%percent%+5
 cls
 echo Microsoft Windows Security Scanner Failed to remove Virus!
+timeout /t 1 >nul
 echo -----------------------------------
 echo.
 echo Corrupting system files...
@@ -29,13 +30,18 @@ echo Progress: %percent%%%
 echo.
 
 if %percent%==15 echo Exploiting C:\Windows\System32...
-if %percent%==30 echo Adding Name To the EPSTIEN Files.Org...
-if %percent%==45 echo Scanning Virus startup programs...
-if %percent%==60 echo Leaking user files...
-if %percent%==75 echo Added Name to the EPSTIEN Files.Org...
-if %percent%==90 echo Releasing the data to the Dark Web ...
-
 timeout /t 1 >nul
+if %percent%==30 echo Adding Name To the EPSTIEN Files.Org...
+timeout /t 1 >nul
+if %percent%==45 echo Scanning Virus startup programs...
+timeout /t 1 >nul
+if %percent%==60 echo Leaking user files...
+timeout /t 1 >nul
+if %percent%==75 echo Added Name to the EPSTIEN Files.Org...
+timeout /t 1 >nul
+if %percent%==90 echo Releasing the data to the Dark Web ...
+timeout /t 1 >nul
+
 
 if %percent%==100 goto result
 goto scan
@@ -45,17 +51,21 @@ cls
 echo Microsoft Windows Security Scanner Failed
 echo -----------------------------------
 echo.
-echo ALERT: 3 Threats Detected!
+echo ALERT: 1 Threats Detected!
 echo.
 echo Trojan.Win32.KeyLogger
+timeout /t 1 >nul
 echo Malware.Injector.Agent
+timeout /t 1 >nul
 echo Suspicious.File.SystemHack
+timeout /t 1 >nul
 echo.
 echo Attempting automatic removal...
 timeout /t 6 >nul
 echo.
 echo ERROR: Removal Failed
 echo.
-echo Just kidding 😄
+timeout /t 6 >nul
+echo Just kidding
 echo You have been pranked!
 pause
