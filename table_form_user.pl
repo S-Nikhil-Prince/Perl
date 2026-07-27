1480e2350626634a83177c389d500cb274b59d07
Nikhil Prince Sodadasi <166209449+S-Nikhil-Prince@users.noreply.github.com>
2026-07-27 20:05:36 +0530

    Fix formatting of prompt message for user input

--- table_form_user.pl
+++ table_form_user.pl
@@ -5,7 +5,7 @@ use feature 'say';
 print"Enter Which Table: ";
 my $val=<STDIN>;
 chomp($val);
-print"Enter upto how many values: ";
+print"Enter upto how many values : ";
 my $val1=<STDIN>;
 chomp($val1);
 for(my $i=1;$i<=$val1;$i++){
